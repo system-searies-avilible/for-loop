8794d8ea1fd4c3baeeddf093279b1fd049acc5fd
system-searies-avilible <111744571+system-searies-avilible@users.noreply.github.com>
2022-08-30 13:59:05 +0400

    Add files via upload

--- file1.php
+++ file1.php
@@ -1,27 +1,12 @@
 <?php
 
-//For loop 
+//Nasted loop
 
-for($a=1; $a<=10; $a++){
-	echo $a.") hello world"."<br/>";
-	
-}
-echo"<br><br>";
-
-for($a=10; $a>=1; $a--){
-	echo$a.") hello world"."<br>";
-}
-
-echo"<br><br>";
-
-for($a=1; $a<=10; $a=$a+2){
-	echo$a.") hello world"."<br>";	
-}
-
-echo"<br><br>";
-
-for($a=0; $a<=10; $a=$a+2){
-	echo$a.") hello world"."<br>";
-	
+for($a=1; $a<=100; $a=$a+10){
+	for($b=$a; $b<$a+10; $b++){
+		echo$b."&nbsp;";
+		
+	}
+	echo"<br>";
 }
 ?>
